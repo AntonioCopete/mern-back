ecdf8edf0da716d92ff37906fb96aa3f7bf29129
AranBeitia <arantza.beitia@gmail.com>
2021-12-28 13:10:36 +0100

    fix cors policy problems

--- src/server.js
+++ src/server.js
@@ -7,25 +7,19 @@ const config = require('./config/config')
 
 const { ProductsRouter, UsersRouter } = require('./routes')
 
-// const UsersRouter = require('./routes/users.routes')
-// const ProductsRouter = require('./routes/products.routes')
-
 const app = express()
 
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(json())
 
-// app.use('/users', UsersRouter)
-// app.use('/products', ProductsRouter)
-
-app.use('/users', UsersRouter)
-app.use('/products', ProductsRouter)
-
 app.use(
   cors({
     origin: config.client.URL,
   })
 )
+
+app.use('/users', UsersRouter)
+app.use('/products', ProductsRouter)
 
 module.exports = app
