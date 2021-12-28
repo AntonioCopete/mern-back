b5abe79bd770dc8e6ad376bf19d0a143a99dc5b2
AntonioCopete <antcopjim@gmail.com>
2021-12-28 18:14:56 +0100

    fix authMiddleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,9 +2,7 @@ const { verifyIdToken } = require('../services/firebase/firebase')
 
 async function authMiddleware(req, res, next) {
   const { authorization } = req.headers
-  console.log(req.headers)
   const token = authorization.slice(7)
-  console.log(token)
   try {
     const verifiedToken = await verifyIdToken(token)
     if (verifiedToken) {
