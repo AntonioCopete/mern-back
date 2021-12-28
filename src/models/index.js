f428951ceca26f03e6995cdb0736e5db23b8c02f
AntonioCopete <antcopjim@gmail.com>
2021-12-28 18:24:54 +0100

    merging changes

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
-const UserModel = require("./user-model");
-const productModel = require("./productModel");
+const UserModel = require('./userModel');
+const productModel = require('./productModel');
 
 module.exports = {
   Product: productModel,
-  User: UserModel
+  User: UserModel,
 };
