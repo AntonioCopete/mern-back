298c24303dc046b42089bc15c60a1b9d2fb4a7dc
Jose1i1o <guadix88@gmail.com>
2021-12-28 10:09:18 +0100

    final stage API

--- src/config/config.js
+++ src/config/config.js
@@ -8,51 +8,7 @@ const ENV = process.env.NODE_ENV || "development";
 logger.enableAll();
 
 const CONFIG = {
-  production: {
-    app: {
-      PORT: process.env.PORT || 4000,
-    },
-    client: {
-      URL: process.env.CLIENT_URL || "http://localhost:4000",
-    },
-    logger: {
-      warn: logger.warn,
-      info: logger.info,
-      error: logger.error,
-      trace: logger.trace,
-      debug: logger.debug,
-    },
-    db: {
-      url: process.env.DB_URL,
-    },
-    firebase: {
-      certConfig: {
-      },
-    },
-  },
   development: {
-    app: {
-      PORT: process.env.PORT || 4000,
-    },
-    client: {
-      URL: process.env.CLIENT_URL || "http://localhost:4000",
-    },
-    logger: {
-      warn: logger.warn,
-      info: logger.info,
-      error: logger.error,
-      trace: logger.trace,
-      debug: logger.debug,
-    },
-    db: {
-      url: process.env.DB_URL,
-    },
-    firebase: {
-      certConfig: {
-      },
-    },
-  },
-  test: {
     app: {
       PORT: process.env.PORT || 4000,
     },
