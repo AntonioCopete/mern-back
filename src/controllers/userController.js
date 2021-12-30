d051201518d71df62db523aee668447848b9d88f
AntonioCopete <antcopjim@gmail.com>
2021-12-30 13:32:24 +0100

    feat login with email and password through firebase

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,38 +1,43 @@
-const db = require('../models')
+const db = require('../models');
 
 async function login(req, res, next) {
-  // const { email } = req.body
-  // try {
-  //   const User = await db.User.findOne({ email: email }).select().lean().exec()
-  //   res.status(200).send({
-  //     success: true,
-  //     data: email,
-  //   })
-  // } catch (err) {
-  //   next(err)
-  // }
+  const { uid, email } = req.user;
+
+  try {
+    const User = await db.User.findOne({ email: email }).select().lean().exec();
+    console.log(User);
+
+    if (User) {
+      res.status(200).send({
+        message: 'Logged successfully',
+        user: User,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 async function createUser(req, res, next) {
-  const { email, password, fullName } = req.body
+  const { email, password, fullName } = req.body;
 
   try {
     const user = await db.User.create({
       fullName: fullName,
       email: email,
       password: password,
-    })
+    });
 
     res.status(201).send({
       success: true,
       data: user,
-    })
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 module.exports = {
   createUser: createUser,
   login: login,
-}
+};
